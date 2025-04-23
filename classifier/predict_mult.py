4cf62c7b90315f529a84d28849175492576c1387
Rickie Ma <rm521@cx3-18-6.cx3.hpc.ic.ac.uk>
2025-04-23 20:22:23 +0100

    add prediction pipeline

--- classifier/predict_mult.py
+++ classifier/predict_mult.py
@@ -8,9 +8,8 @@ def preprocess_function(examples):
     examples['log'] = [str(text) for text in examples['log']]
     return tokenizer(examples['log'], truncation=True, padding='max_length', max_length=512, return_tensors="pt")
 
-# load logs
 lines = []
-with open("../data/log/ssl-access.log-20181008", "r") as f:
+with open("../data/log/ssl-access.log-20181009", "r") as f:
     lines.extend(f.readlines())
 
 # Create a DataFrame where each line is a row
@@ -20,24 +19,26 @@ df = pd.DataFrame(lines, columns=["log"])
 df["log"] = df["log"].str.strip()
 dataset = Dataset.from_pandas(df)
 
-model_path = "/rds/general/user/rm521/home/fyp/data/checkpoint-3392"
-model = DebertaV2ForSequenceClassification.from_pretrained(model_path, num_labels=2, output_attentions=False, output_hidden_states=False)
+model_path = "/rds/general/user/rm521/home/fyp/classifier/2304_12-57/checkpoint-8032"
+model = DebertaV2ForSequenceClassification.from_pretrained(model_path, num_labels=10, output_attentions=False, output_hidden_states=False)
 model.cuda()
 tokenizer = DebertaV2Tokenizer.from_pretrained(model_path)
 
 tokenized_valid = dataset.map(preprocess_function, batched=True)
-dataloader = DataLoader(tokenized_valid, batch_size=220, collate_fn=default_data_collator)
+dataloader = DataLoader(tokenized_valid, batch_size=260, collate_fn=default_data_collator)
 
 model.eval()
-
 result = []
 with torch.no_grad():
     for batch in dataloader:
         input_ids = batch["input_ids"].cuda()
         attention_mask = batch["attention_mask"].cuda()
         outputs = model(input_ids=input_ids, attention_mask=attention_mask)
-        predictions = torch.argmax(outputs.logits, dim=1)
+        probs = torch.sigmoid(outputs.logits)
+        predictions = (probs > 0.5).int()
         result.extend(predictions.cpu().tolist())
 
-filtered = df[df['pred']==1]
+labels = [[i for i, val in enumerate(row) if val == 1] for row in result]
+df['pred'] = labels
+filtered = df[df['pred'].apply(lambda x: x != [0])]
 filtered.to_csv("acunetix.csv", index=False)
